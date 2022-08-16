f645d92694166f749ad686e824c845a167f2cee8
Abdul Rehman <mehars.6925@gmail.com>
2022-08-17 03:07:19 +0500

    registered express app to the server

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config();
+
 const { PORT } = process.env;
 const http = require('http');
 const slackApp = require('./slack');
+const expressApp = require('./express');
 
-const server = http.createServer();
+const server = http.createServer(expressApp);
 
 server.listen(PORT || 3000, async () => {
   await slackApp.start(PORT || 3000);
